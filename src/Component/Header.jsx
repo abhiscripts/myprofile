9dfcabb862ed697826141352d3151ba9fbf2c132
Abhishek <abhishek.kc6@gmail.com>
2023-06-05 20:08:40 +0530

    updated img

--- src/Component/Header.jsx
+++ src/Component/Header.jsx
@@ -2,10 +2,10 @@ import React from "react";
 import EmailIcon from "@mui/icons-material/Email";
 import LinkedInIcon from "@mui/icons-material/LinkedIn";
 import GitHubIcon from "@mui/icons-material/GitHub";
-import BurstModeIcon from '@mui/icons-material/BurstMode';
+import BurstModeIcon from "@mui/icons-material/BurstMode";
 import cert from "./../Assets/cert.png";
 import logo from "./../Assets/logo.png";
-import { maxHeight, style } from "@mui/system";
+import safe from "./../Assets/safe.png";
 
 const SOCIAL_LINKS = {
   email: "mailto:abhishek.kc6@gmail.com",
@@ -45,17 +45,18 @@ export default function Header({ projectsRef, contactRef }) {
           onClick={() => handleIconClick(SOCIAL_LINKS["medium"])}
           style={{ height: "100px" }}
         />
-      </div>
-      <div className="headerPage">
-        <button onClick={scrollToProjects}>Projects</button>
-        <button onClick={scrollToContact}>Contact Me</button>
-        {/* <a
+        <a
           target="_blank"
           rel="noreferrer"
           href="https://www.credly.com/badges/a428a2ea-82c4-4d3e-a5cf-f492757b3403/public_url"
         >
-          <img src={cert} alt="meta cert"/>
-        </a> */}
+          <img src={cert} alt="meta cert" style={{ height: "70px", paddingTop:"15px", paddingLeft: "45px" }}/>
+        </a>
+        <img src={safe} alt="safe cert" style={{ height: "80px", paddingTop:"12px", paddingLeft: "5px" }}/>
+      </div>
+      <div className="headerPage">
+        <button onClick={scrollToProjects}>Projects</button>
+        <button onClick={scrollToContact}>Contact Me</button>
       </div>
     </div>
   );
