5b8b18c2e46f1216ae4fd0ec44965e422e479934
Abhishek <abhishek.kc6@gmail.com>
2023-06-05 20:24:37 +0530

    updated img

--- src/Component/About.jsx
+++ src/Component/About.jsx
@@ -1,6 +1,9 @@
 import React from "react";
 import Skills from "./Skills";
 import me from "../Assets/me.jpg";
+import cert from "./../Assets/cert.png";
+import logo from "./../Assets/logo.png";
+import safe from "./../Assets/safe.png";
 
 export default function About() {
   return (
@@ -12,7 +15,24 @@ export default function About() {
         <br />
         With 7+ years of experience in Aircraft, Media and Automotive Domains.
       </h1>
-      <Skills/>
+      <div className="headorLogos">
+        <img src={logo} alt="logo" style={{ maxHeight: "90px" } }/>
+          <a
+            target="_blank"
+            rel="noreferrer"
+            href="https://www.credly.com/badges/a428a2ea-82c4-4d3e-a5cf-f492757b3403/public_url"
+          >
+            <img
+              src={cert}
+              alt="meta cert"
+              style={{
+                maxHeight: "100px",
+              }}
+            />
+          </a>
+          <img src={safe} alt="safe cert" style={{ maxHeight: "90px" }} />
+        </div>
+        <Skills/>
     </div>
   );
 }
--- src/Component/Header.jsx
+++ src/Component/Header.jsx
@@ -3,9 +3,6 @@ import EmailIcon from "@mui/icons-material/Email";
 import LinkedInIcon from "@mui/icons-material/LinkedIn";
 import GitHubIcon from "@mui/icons-material/GitHub";
 import BurstModeIcon from "@mui/icons-material/BurstMode";
-import cert from "./../Assets/cert.png";
-import logo from "./../Assets/logo.png";
-import safe from "./../Assets/safe.png";
 
 const SOCIAL_LINKS = {
   email: "mailto:abhishek.kc6@gmail.com",
@@ -27,33 +24,24 @@ export default function Header({ projectsRef, contactRef }) {
 
   return (
     <div className="header">
-      <div className="headerSocial">
-        <img src={logo} alt="logo" />
-        <EmailIcon
-          onClick={() => handleIconClick(SOCIAL_LINKS["email"])}
-          style={{ height: "100px" }}
-        />
-        <LinkedInIcon
-          onClick={() => handleIconClick(SOCIAL_LINKS["linkedin"])}
-          style={{ height: "100px" }}
-        />
-        <GitHubIcon
-          onClick={() => handleIconClick(SOCIAL_LINKS["github"])}
-          style={{ height: "100px" }}
-        />
-        <BurstModeIcon
-          onClick={() => handleIconClick(SOCIAL_LINKS["medium"])}
-          style={{ height: "100px" }}
-        />
-        <a
-          target="_blank"
-          rel="noreferrer"
-          href="https://www.credly.com/badges/a428a2ea-82c4-4d3e-a5cf-f492757b3403/public_url"
-        >
-          <img src={cert} alt="meta cert" style={{ height: "70px", paddingTop:"15px", paddingLeft: "45px" }}/>
-        </a>
-        <img src={safe} alt="safe cert" style={{ height: "80px", paddingTop:"12px", paddingLeft: "5px" }}/>
-      </div>
+        <div className="headerSocial">
+          <EmailIcon
+            onClick={() => handleIconClick(SOCIAL_LINKS["email"])}
+            style={{ height: "100px" }}
+          />
+          <LinkedInIcon
+            onClick={() => handleIconClick(SOCIAL_LINKS["linkedin"])}
+            style={{ height: "100px" }}
+          />
+          <GitHubIcon
+            onClick={() => handleIconClick(SOCIAL_LINKS["github"])}
+            style={{ height: "100px" }}
+          />
+          <BurstModeIcon
+            onClick={() => handleIconClick(SOCIAL_LINKS["medium"])}
+            style={{ height: "100px" }}
+          />
+        </div>
       <div className="headerPage">
         <button onClick={scrollToProjects}>Projects</button>
         <button onClick={scrollToContact}>Contact Me</button>
